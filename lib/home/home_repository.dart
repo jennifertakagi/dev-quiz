8253f0b4788c3f1cbd7ed944fe4f5470ac108a96
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 14:25:11 -0300

    feat: add HomeRepository to handle to database

---
+++ lib/home/home_repository.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+import 'package:DevQuiz/shared/shared.dart';
+
+class HomeRepository {
+  Future<UserModel> getUser() async {
+    final response = await rootBundle.loadString("assets/database/user.json");
+    final user = UserModel.fromJson(response);
+
+    return user;
+  }
+
+  Future<List<QuizModel>> getQuizzes() async {
+    final response =
+        await rootBundle.loadString("assets/database/quizzes.json");
+    final list = jsonDecode(response) as List;
+    final quizzes = list.map((e) => QuizModel.fromMap(e)).toList();
+
+    return quizzes;
+  }
+}
