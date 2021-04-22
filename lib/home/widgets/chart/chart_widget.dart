bc2b5800b774f93b15d5066898f2a941fe5539d3
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 09:27:35 -0300

    feat: add ChartWidget

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import '../../../core/core.dart';
 
 class ChartWidget extends StatelessWidget {
-  ChartWidget({Key? key}) : super(key: key);
+  const ChartWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
