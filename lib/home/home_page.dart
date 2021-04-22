3cd8a1ec4490bdcfa866358c06124750f9040933
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 14:26:02 -0300

    feat: adjust HomePage to get all information dynamic

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,7 +1,9 @@
-import 'package:DevQuiz/home/widgets/app_bar/app_bar_widget.dart';
-import 'package:DevQuiz/home/widgets/level_button/level_button_widget.dart';
-import 'package:DevQuiz/home/widgets/quiz_card/quiz_card_widget.dart';
+import 'package:DevQuiz/core/core.dart';
 import 'package:flutter/material.dart';
+
+import 'home_controller.dart';
+import 'home_state.dart';
+import 'widgets/widgets_home.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,54 +13,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+
   @override
+  void initState() {
+    super.initState();
+    controller.getUser();
+    controller.getQuizzes();
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                LevelButtonWidget(
-                  label: "Easy",
-                ),
-                LevelButtonWidget(
-                  label: "Medium",
-                ),
-                LevelButtonWidget(
-                  label: "Hard",
-                ),
-                LevelButtonWidget(
-                  label: "Expert",
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: GridView.count(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
+    if (controller.state == HomeState.sucess) {
+      return Scaffold(
+        appBar: AppBarWidget(user: controller.user!),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  QuizCardWidget(),
-                  QuizCardWidget(),
-                  QuizCardWidget(),
+                  LevelButtonWidget(
+                    label: "Easy",
+                  ),
+                  LevelButtonWidget(
+                    label: "Medium",
+                  ),
+                  LevelButtonWidget(
+                    label: "Hard",
+                  ),
+                  LevelButtonWidget(
+                    label: "Expert",
+                  ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  children: controller.quizzes!
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          questionCompleted:
+                              "${e.questionAnswered}/${e.questions.length}",
+                          questionCompletedPorcents:
+                              e.questionAnswered / e.questions.length,
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+          ),
+        ),
+      );
+    }
   }
 }
