8503513af20b996e137d933164a7f2825cc29900
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 11:16:35 -0300

    feat: create application Models

---
+++ lib/shared/models/quiz_model.dart
@@ -0,0 +1,24 @@
+import 'package:DevQuiz/shared/models/question_model.dart';
+
+enum Level {
+  easy,
+  medium,
+  hard,
+  expert,
+}
+
+class QuizModel {
+  final String title;
+  final List<QuestionModel> questions;
+  final int questionAnswered;
+  final String image;
+  final Level level;
+
+  QuizModel({
+    required this.title,
+    required this.questions,
+    this.questionAnswered = 0,
+    required this.image,
+    required this.level,
+  });
+}
---
+++ lib/shared/shared.dart
@@ -0,0 +1,6 @@
+export './models/answer_model.dart';
+export './models/question_model.dart';
+export './models/quiz_model.dart';
+export './models/user_model.dart';
+
+export './widgets/progress_indicator/progress_indicator_widget.dart';
