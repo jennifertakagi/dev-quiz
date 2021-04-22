49e91320b8f9f4522395b1b42f5312096ea014c4
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 14:23:38 -0300

    feat: add jsonSerialized method on models

--- lib/shared/models/quiz_model.dart
+++ lib/shared/models/quiz_model.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:DevQuiz/shared/models/question_model.dart';
 
 enum Level {
@@ -5,6 +7,24 @@ enum Level {
   medium,
   hard,
   expert,
+}
+
+extension LevelStringExt on String {
+  Level get parse => {
+        "easy": Level.easy,
+        "medium": Level.medium,
+        "hard": Level.hard,
+        "expert": Level.expert,
+      }[this]!;
+}
+
+extension LevelExt on Level {
+  String get parse => {
+        Level.easy: "easy",
+        Level.medium: "medium",
+        Level.hard: "hard",
+        Level.expert: "expert",
+      }[this]!;
 }
 
 class QuizModel {
@@ -21,4 +41,30 @@ class QuizModel {
     required this.image,
     required this.level,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'questions': questions.map((x) => x.toMap()).toList(),
+      'questionAnswered': questionAnswered,
+      'image': image,
+      'level': level.parse,
+    };
+  }
+
+  factory QuizModel.fromMap(Map<String, dynamic> map) {
+    return QuizModel(
+      title: map['title'],
+      questions: List<QuestionModel>.from(
+          map['questions']?.map((x) => QuestionModel.fromMap(x))),
+      questionAnswered: map['questionAnswered'],
+      image: map['image'],
+      level: map['level'].toString().parse,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuizModel.fromJson(String source) =>
+      QuizModel.fromMap(json.decode(source));
 }
