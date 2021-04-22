3c7ddb046abea20afdadaf9be4d7bd07e095597b
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 09:27:48 -0300

    feat: add QuestionIndicatorWidget

---
+++ lib/challenge/widget/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,37 @@
+import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Question 04",
+                style: AppTextStyles.body,
+              ),
+              Text(
+                "04/10",
+                style: AppTextStyles.body,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          ProgressIndicatorWidget(
+            value: 0.7,
+          ),
+        ],
+      ),
+    );
+  }
+}
