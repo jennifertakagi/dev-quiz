08ce407517a8f5cc573873a46eb6f0c84586ff63
jennifertakagi <jennitakagi@gmail.com>
2021-04-22 09:28:52 -0300

    feat: add ChallengePage

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,28 @@
+import 'package:DevQuiz/challenge/widget/question_indicator/question_indicator_widget.dart';
+import 'package:DevQuiz/challenge/widget/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  const ChallengePage({Key? key}) : super(key: key);
+
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(
+          top: true,
+          child: QuestionIndicatorWidget(),
+        ),
+      ),
+      body: QuizWidget(
+        title: "What Flutter do?",
+      ),
+    );
+  }
+}
