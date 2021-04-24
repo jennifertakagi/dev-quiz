40c43c33baf476084fc94f15c2a3f850c340863b
jennifertakagi <jennitakagi@gmail.com>
2021-04-24 18:04:19 -0300

    chore: export widget file and add more question on json

--- lib/challenge/widget/widgets_challenge.dart
+++ lib/challenge/widget/widgets_challenge.dart
@@ -1,3 +1,4 @@
 export './answer/answer_widget.dart';
+export './next_button/next_button_widget.dart';
 export './question_indicator/question_indicator_widget.dart';
 export './quiz/quiz_widget.dart';
