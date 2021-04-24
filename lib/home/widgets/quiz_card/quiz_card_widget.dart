4e50713cd68f179e67ee43df8e7b28ff0001e7ea
jennifertakagi <jennitakagi@gmail.com>
2021-04-24 18:04:50 -0300

    feat: add onTap behavior on QuizCard Widget

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -7,63 +7,68 @@ class QuizCardWidget extends StatelessWidget {
   final String title;
   final String questionCompleted;
   final double questionCompletedPorcents;
+  final VoidCallback onTap;
 
   const QuizCardWidget({
     Key? key,
     required this.title,
     required this.questionCompleted,
     required this.questionCompletedPorcents,
+    required this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        border: Border.fromBorderSide(
-          BorderSide(
-            color: AppColors.border,
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.fromBorderSide(
+            BorderSide(
+              color: AppColors.border,
+            ),
           ),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10),
         ),
-        color: AppColors.white,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 40,
-            width: 40,
-            child: Image.asset(AppImages.blocks),
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Text(
-            title,
-            style: AppTextStyles.heading15,
-          ),
-          SizedBox(
-            height: 24,
-          ),
-          Row(
-            children: [
-              Expanded(
-                flex: 1,
-                child: Text(
-                  questionCompleted,
-                  style: AppTextStyles.body11,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              child: Image.asset(AppImages.blocks),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              title,
+              style: AppTextStyles.heading15,
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(
+                    questionCompleted,
+                    style: AppTextStyles.body11,
+                  ),
                 ),
-              ),
-              Expanded(
-                flex: 4,
-                child: ProgressIndicatorWidget(
-                  value: questionCompletedPorcents,
+                Expanded(
+                  flex: 4,
+                  child: ProgressIndicatorWidget(
+                    value: questionCompletedPorcents,
+                  ),
                 ),
-              ),
-            ],
-          )
-        ],
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
