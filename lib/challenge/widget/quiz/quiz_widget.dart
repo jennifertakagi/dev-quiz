3a22dd120662662979fec159ff844dca118a5b4b
jennifertakagi <jennitakagi@gmail.com>
2021-04-24 18:02:56 -0300

    feat: adjust QuizWidget to get dynamic data

--- lib/challenge/widget/quiz/quiz_widget.dart
+++ lib/challenge/widget/quiz/quiz_widget.dart
@@ -1,36 +1,55 @@
-import 'package:DevQuiz/challenge/widget/answer/answer_widget.dart';
-import 'package:DevQuiz/core/core.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+import 'package:DevQuiz/challenge/widget/answer/answer_widget.dart';
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/shared/shared.dart';
+
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+
+  const QuizWidget({
+    Key? key,
+    required this.question,
+    required this.onChange,
+  }) : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.answers[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerWidget(
-            isRight: true,
-            isSelected: true,
-            title: "Possibilita a criação",
-          ),
-          AnswerWidget(
-            isRight: false,
-            isSelected: true,
-            title: "Possibilita a criação",
-          ),
-          AnswerWidget(
-            title: "Possibilita a criação",
-          ),
+          for (var i = 0; i < widget.question.answers.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+                setState(() {});
+                Future.delayed(Duration(seconds: 1)).then(
+                  (value) => widget.onChange(),
+                );
+              },
+            ),
         ],
       ),
     );
