04679c66c8fa234f8ff4c5f0a89089d6d9cc97e7
jennifertakagi <jennitakagi@gmail.com>
2021-04-24 19:37:46 -0300

    feat: add corretedQuestion on challenge controller

--- lib/challenge/challenge_controller.dart
+++ lib/challenge/challenge_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class ChallengeController {
   final currentQuestionNotifier = ValueNotifier<int>(1);
+  int corretedQuestions = 0;
 
   int get currentQuestion => currentQuestionNotifier.value;
   set currentQuestion(int value) => currentQuestionNotifier.value = value;
